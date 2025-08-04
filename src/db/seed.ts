75165dc69eb41cd29e5e59ba4af5e399ac93ef3d
larissaorcx <larissaorcx@gmail.com>
2025-08-04 09:34:15 -0300

    🚧 feat: config para popular a tabela rooms com dados fictícios

---
+++ src/db/seed.ts
@@ -0,0 +1,26 @@
+//populando a tabela com dados fictícios
+
+import { reset, seed } from 'drizzle-seed';
+import { db, sql } from './connection.ts';
+import { schema } from './schema/index.ts';
+
+await reset(db, schema); //limpando o db
+
+await seed(db, schema).refine((f) => {
+  //populando a tabela rooms com dados fictícios
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        //colunas da tabela
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  };
+});
+
+await sql.end();
+
+// biome-ignore lint/suspicious/noConsole: only used in dev
+console.log('Database seeded');
